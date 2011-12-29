5c5d2e24bf344a82308a3eed29d57630b0d1aedc
myrobots <casmat@robotshop.com>
2011-12-29 15:48:10 -0500

    We finally got a github account

---
+++ Supported Devices/Python Sample/Send_Random_Robot_Data.py
@@ -0,0 +1,107 @@
+## By: Carlos Asmat for MyRobots.com
+## Date: 29/12/2011
+##
+## Description: Simple script to define a Robot object,
+## instantiate many of them, fill them with random data
+## and syncronysze them with MyRobots.com
+
+## Import required libraries
+import httplib, urllib, random, time
+
+class Robot:
+    """Robot Class defines the parameters of a robot 
+and MyRobots.com sync function"""
+    # Latest robot data is stored in parameters dictionary
+    parameters = {}
+
+    def __init__(self, key):
+        """Initialises parameters dictionary and sets teh robot key."""
+        self.parameters = {'field1': 0, 'field2': 0, 'field3': 0, 'field4': 0,
+                  'field5': 0, 'field6': 0, 'field7': 0, 'field8': 0,
+                  'key':'000000000000000', 'status':'operational'}
+        self.parameters['key'] = key
+    
+    def sync(self):
+        """Syncs your robot information and prints teh server response."""
+        params = urllib.urlencode(self.parameters)
+        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("bots.myrobots.com")
+        conn.request("POST", "/update", params, headers)
+        response = conn.getresponse()
+        print response.status, response.reason
+        data = response.read()
+        conn.close()
+
+##################################################################
+## Parameters you can edit
+##################################################################
+
+## List of status messages, define your own.
+## They will be randomly selected and sent to MyRobots.com
+status_msg = ['Happy',
+              'Sad',
+              'Cleaning',
+              'Charging',
+              'Stuck',
+              'Confused',
+              "Here I am, brain the size of a planet, \
+and they ask me to take you to the bridge. \
+Call that job satisfaction, 'cause I don't.",
+              "I think you ought to know I'm feeling very depressed.",
+              "I've calculated your chance of survival, \
+but I don't think you'll like it. ",
+              "Hasta la vista baby.",
+              "EXTREMINATE!",
+              "DELETE!",
+              "MyRobots.com is Live!! Finaly robots can ge a social life",
+              "Come with me if you want to live"]
+
+## Add the keys for your robots.
+## Obtain teh keys at MyRobots.com
+keys = ['YOUR_KEY_HERE',
+        'YOUR_KEY_HERE',
+        'YOUR_KEY_HERE',
+        'YOUR_KEY_HERE']
+
+## Set the time between updates
+period = 60;
+
+## Set the number of tiems you wnat your robots to be updated
+times = 100
+
+
+##################################################################
+## Sending Robot data to Myrobots.com
+##################################################################
+
+## Period shuld be > 60s.
+if period < 60:
+    period = 60
+
+## Instantiate a robot for every key in keys.
+robots = [Robot(k) for k in keys]
+
+
+## Make several updates (as many as specified in times).
+for i in range(times):
+    print i ## Print itteration number for debugging purposes.
+    for r in robots:
+        ## Fill parameters with random data
+        r.parameters['field1'] = random.randint(10, 40)
+        r.parameters['field2'] = random.random()
+        r.parameters['field3'] = random.randint(-30, 30)
+        r.parameters['field4'] = random.randint(0, 1)
+        r.parameters['field5'] = random.randint(0, 1)
+        r.parameters['field6'] = random.randint(10, 40)
+        r.parameters['field7'] = random.randint(10, 40)
+        r.parameters['field8'] = random.randint(10, 40)
+        r.parameters['status'] = random.choice(status_msg)
+
+        ## Print the robot key for debugging purposes.
+        print r.parameters['key']
+
+        ## send robot data to MyRobots.com
+        r.sync()
+
+    print '\n\n' ## Print white space number for debugging purposes.
+    time.sleep(period)
